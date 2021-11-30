8e244f034bfd05e7d908fda476c4b222fbdff33a
barhomalko <90499995+barhomalko@users.noreply.github.com>
2021-11-29 20:02:44 -0500

    Add files via upload

--- ch4e7.h
+++ ch4e7.h
@@ -53,3 +53,46 @@ int get_number()
     // only executes if input is incorrect
     return(-1);
 }
+
+
+
+int main()
+{
+    init_vector();
+    cout << "Enter two values (number or single digit spelled out) separated by\n"
+        << "a character (+, -, * or /). Enter 'q' as operator to exit.\n";
+    while (true) {
+        int val1 = 0;
+        int val2 = 0;
+        char op = 0;
+        val1 = get_number();
+        cin >> op;
+        val2 = get_number();
+        if (val1==-1 || val2==-1)
+            cout << "Numbers must be integers or single digits spelled out\n";
+        else {
+            switch (op) {
+            case '+':
+                cout << "The sum of " << val1 << " and " << val2 << " is " << val1+val2 << ".\n";
+                break;
+            case '-':
+                cout << "The difference of " << val1 << " and " << val2 << " is " << val1-val2 << ".\n";
+                break;
+            case '*':
+                cout << "The product of " << val1 << " and " << val2 << " is " << val1*val2 << ".\n";
+                break;
+            case '/':
+                if (val2==0)
+                    cout << "Indivisible by zero!\n";
+                else
+                    cout << "The ratio of " << val1 << " and " << val2 << " is " << double(val1)/val2 << ".\n";
+                break;
+            case 'q':
+                return 0;
+            default:
+                cout << "This is not a valid operation (use +, -, * or /)\n";
+                break;
+            }
+        }
+    }
+}
